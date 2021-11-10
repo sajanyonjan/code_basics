7115e85cef40e22ada06dd6415e5aff9ee9ea68b
sajan tamang <sajanyonjan14@gmail.com>
2021-11-10 14:07:26 +0545

    rest

--- hungry.py
+++ hungry.py
@@ -5,6 +5,10 @@ if hungry=="yes":
     print("eat fries")
 else:
     thirsty=input('are you thirsty?')
+    sit=input('do you want to rest?')
     if thirsty=="yes":
         print("drink water")
         print("drink soda")
+    elif sit=='yes':
+        print('take rest')
+        print('You need rest')
